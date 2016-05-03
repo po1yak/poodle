6da550659685aa51741c7e8966ead6192ecd7927
shkadov <shkadov@gmail.com>
2016-05-03 12:36:25 +0300

    Fixed

--- Disabling_SSLv3.sh
+++ Disabling_SSLv3.sh
@@ -7,14 +7,14 @@
 
 case "${1}" in
         outputFormat)
-                echo "HTTPS|cPanel SSL|IMAP4 SSL|FTP SSL|"
+                echo "Apache|WHM|Dovecot|Courier|FTP SSL|Exim|FDisk"
         ;;
         *)
 
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/apache/local")
 	if [ ! -f "/var/cpanel/conf/apache/local" ]
 	then 
-		file=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "touch /var/cpanel/conf/apache/local && echo "SSLProtocol All -SSLv2 -SSLv3" > /var/cpanel/conf/apache/local ") 
+		ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "touch /var/cpanel/conf/apache/local && echo "SSLProtocol All -SSLv2 -SSLv3" > /var/cpanel/conf/apache/local "
 		output=$(echo "Created")
 	else
 		entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSLProtocol all.*" /var/cpanel/conf/apache/local")
@@ -24,7 +24,7 @@ case "${1}" in
 		output=$(echo "Changed")
 		fi
  	fi
-	service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/scripts/rebuildhttpdconf && service httpd restart")
+		ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/scripts/rebuildhttpdconf && service httpd restart"
 
 
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/cpsrvd/ssl_socket_args")
@@ -34,7 +34,7 @@ case "${1}" in
                 change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/SSL_version */SSL_version SSLv23:!SSLv2:!SSLv3/' /var/cpanel/conf/cpsrvd/ssl_socket_args")
                 output=$(echo "Changed")
                 fi
-        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 docpsrvdconfiguration")
+	        ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 docpsrvdconfiguration"
 
 
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/dovecot/main")
@@ -44,7 +44,7 @@ case "${1}" in
                 change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/SSL_protocol */SSL_protocol !SSLv2 !SSLv3/' /var/cpanel/conf/dovecot/main")
                 output=$(echo "Changed")
                 fi
-        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 savedovecotsetup")
+        	ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 savedovecotsetup"
 
 
 	status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/pureftpd/main")
@@ -54,7 +54,7 @@ case "${1}" in
                 change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/TLSCipherSuite */TLSCipherSuite  HIGH:MEDIUM:+TLSv1:!SSLv2:!SSLv3/' /var/cpanel/conf/pureftpd/main")
                 output=$(echo "Changed")
                 fi
-        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/bin/build_ftp_conf && service pure-ftpd restart")
+        	ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/bin/build_ftp_conf && service pure-ftpd restart"
 
         ;;
 esac
