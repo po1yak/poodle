5816a67624b2d579655da3ed0a3393375cbcec63
shkadov <shkadov@gmail.com>
2016-05-03 09:54:33 +0300

    Added Pureftpd

--- Disabling_SSLv3.sh
+++ Disabling_SSLv3.sh
@@ -20,42 +20,37 @@ case "${1}" in
 		if [ "x${entry}" = "x" ]
 		then 
 		change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/SSLProtocol all.*/SSLProtocol All -SSLv2 -SSLv3/' /var/cpanel/conf/apache/local")
-		output=$(echo "")
+		output=$(echo "Changed")
 		fi
  	fi
 	service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/scripts/rebuildhttpdconf && service httpd restart")
-	;;
-esac
-
-case "${2}" in
-        outputFormat)
-                echo "Status1"
-        ;;
-        *)
-        status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${2} "cat /var/cpanel/conf/cpsrvd/ssl_socket_args")
-                entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${2} "grep -i "SSL_version *" /var/cpanel/conf/cpsrvd/ssl_socket_args")
+        status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/cpsrvd/ssl_socket_args")
+                entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSL_version *" /var/cpanel/conf/cpsrvd/ssl_socket_args")
                 if [ "x${entry}" != "SSL_version SSLv23:!SSLv2:!SSLv3" ]
                 then
-                change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${2} "sed -i 's/SSL_version */SSL_version SSLv23:!SSLv2:!SSLv3/' /var/cpanel/conf/cpsrvd/ssl_socket_args")
-                output=$(echo "")
+                change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/SSL_version */SSL_version SSLv23:!SSLv2:!SSLv3/' /var/cpanel/conf/cpsrvd/ssl_socket_args")
+                output=$(echo "Changed")
                 fi
-        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${2} "/usr/local/cpanel/whostmgr/bin/whostmgr2 docpsrvdconfiguration")
-        ;;
-esac
+        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 docpsrvdconfiguration")
 
-case "${3}" in
-        outputFormat)
-                echo "Status2"
-        ;;
-        *)
-        status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${3} "cat /var/cpanel/conf/dovecot/main")
-                entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${3} "grep -i "SSL_protocol *" /var/cpanel/conf/dovecot/main")
+        status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/dovecot/main")
+                entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSL_protocol *" /var/cpanel/conf/dovecot/main")
                 if [ "x${entry}" != "SSL_protocol !SSLv2 !SSLv3" ]
                 then
-                change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${3} "sed -i 's/SSL_protocol */SSL_protocol !SSLv2 !SSLv3/' /var/cpanel/conf/dovecot/main")
-                output=$(echo "")
+                change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/SSL_protocol */SSL_protocol !SSLv2 !SSLv3/' /var/cpanel/conf/dovecot/main")
+                output=$(echo "Changed")
                 fi
-        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${3} "/usr/local/cpanel/whostmgr/bin/whostmgr2 savedovecotsetup")
+        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 savedovecotsetup")
+
+	status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/pureftpd/main")
+                entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "TLSCipherSuite" /var/cpanel/conf/pureftpd/main")
+                if [ "x${entry}" != "HIGH:MEDIUM:+TLSv1:!SSLv2:!SSLv3" ]
+                then
+                change=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "sed -i 's/TLSCipherSuite */TLSCipherSuite  HIGH:MEDIUM:+TLSv1:!SSLv2:!SSLv3/' /var/cpanel/conf/pureftpd/main")
+                output=$(echo "Changed")
+                fi
+        service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/bin/build_ftp_conf && service pure-ftpd restart")
+
         ;;
 esac
 
