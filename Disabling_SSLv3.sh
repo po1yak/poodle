2119b2b169c0d85e782988688128fc35266a316f
shkadov <shkadov@gmail.com>
2016-05-03 12:16:23 +0300

    Corrected Status

--- Disabling_SSLv3.sh
+++ Disabling_SSLv3.sh
@@ -7,14 +7,15 @@
 
 case "${1}" in
         outputFormat)
-                echo "Status"
+                echo "HTTPS|cPanel SSL|IMAP4 SSL|FTP SSL|"
         ;;
         *)
+
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/apache/local")
 	if [ ! -f "/var/cpanel/conf/apache/local" ]
 	then 
 		file=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "touch /var/cpanel/conf/apache/local && echo "SSLProtocol All -SSLv2 -SSLv3" > /var/cpanel/conf/apache/local ") 
-		output=$(echo "")
+		output=$(echo "Created")
 	else
 		entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSLProtocol all.*" /var/cpanel/conf/apache/local")
 		if [ "x${entry}" = "x" ]
@@ -24,6 +25,8 @@ case "${1}" in
 		fi
  	fi
 	service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/scripts/rebuildhttpdconf && service httpd restart")
+
+
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/cpsrvd/ssl_socket_args")
                 entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSL_version *" /var/cpanel/conf/cpsrvd/ssl_socket_args")
                 if [ "x${entry}" != "SSL_version SSLv23:!SSLv2:!SSLv3" ]
@@ -33,6 +36,7 @@ case "${1}" in
                 fi
         service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 docpsrvdconfiguration")
 
+
         status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/conf/dovecot/main")
                 entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "SSL_protocol *" /var/cpanel/conf/dovecot/main")
                 if [ "x${entry}" != "SSL_protocol !SSLv2 !SSLv3" ]
@@ -41,6 +45,7 @@ case "${1}" in
                 output=$(echo "Changed")
                 fi
         service=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "/usr/local/cpanel/whostmgr/bin/whostmgr2 savedovecotsetup")
+
 
 	status=$(ssh -q -o ConnectTimeout=20 -o StrictHostKeyChecking=no ${1} "cat /var/cpanel/pureftpd/main")
                 entry=$(ssh -q -o ConnectTimeOut=20 -o StrictHostKeyChecking=no ${1} "grep -i "TLSCipherSuite" /var/cpanel/conf/pureftpd/main")
